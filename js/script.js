c837080e36c89f2c22e0503e30614bbd7e242612
Clara Ribeiro <clara2020flor@gmail.com>
2024-07-10 18:52:39 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,114 @@
+function loadElementsHome(){
+    loadHeader("home");
+    panelSlides(0);
+    loadFooter("home");
+}
+function loadElementsAbout(){
+    loadHeader("about");
+    loadFooter("about");
+}
+
+function loadHeader(pageName) {
+    document.getElementById('header-place').innerHTML = (`
+        <div class="logo">
+            <p>by</p>
+            <img class="button" src="img/logo.svg" alt="logo image" onclick="window.open('https://github.com/ClaraGRibeiro', '_blank')" title="GitHub">
+        </div>
+        <p class="code">&lt;${pageName}&gt;</p>
+        <nav class="buttons">
+            <img class="button" src="img/home.svg" alt="home image" onclick="window.open('index.html', '_self')" title="Home">
+            <img class="button" src="img/code.svg" alt="code image" onclick="window.open('https://github.com/ClaraGRibeiro', '_blank')" title="Code"><!--COLOCAR LINK DO GITHUB-->
+            <img class="button" src="img/about.svg" alt="about image" onclick="window.open('about.html', '_self')" title="About">
+        </nav>
+    `);
+}
+function loadFooter(pageName) {
+    document.getElementById('footer-place').innerHTML = (`
+        <div class="logo">
+            <p>by</p>
+            <img class="button" src="img/logo.svg" alt="logo image" onclick="window.open('https://github.com/ClaraGRibeiro', '_blank')" title="GitHub">
+        </div>
+        <p class="code">&lt;/${pageName}&gt;</p>
+        <nav class="buttons">
+            <img class="button" src="img/github.svg" alt="github image" onclick="window.open('https://github.com/ClaraGRibeiro', '_blank')" title="GitHub">
+            <img class="button" src="img/linkedin.svg" alt="linkedin image" onclick="window.open('https://www.linkedin.com/in/clara-gon%C3%A7alves-ribeiro-66b07a213/', '_blank')" title="LinkedIn">
+            <img class="button" src="img/email.svg" alt="email image" onclick="window.open('https://mail.google.com/mail/u/0/?fs=1&to=clara2020flor@gmail.com&su=FeedBack+for+%27Playing+With+CSS%27&body=Hi,+my+name+is+%5BNAME%5D+and+I+am+from+%5BCOUNTRY%5D.%0D%0AI+found+a+mistake:+%5BMISTAKE+EXPLAINED%5D.%0D%0A%0D%0A%0D%0A...&tf=cm', '_blank')" title="Send me a feedback">
+        </nav>
+    `);
+}
+
+
+slideCount = 0;
+totalSlide = 4;
+function previous(){
+    if(slideCount > 1){
+        slideCount--;
+    } else{
+        slideCount = totalSlide;
+    }
+    panelSlides(slideCount);
+}
+function next(){
+    if(slideCount < totalSlide){
+        slideCount++;
+    } else{
+        slideCount = 1;
+    }
+    panelSlides(slideCount);
+}
+function panelSlides(number){
+    slideCount = number;
+    slideFile = '../slides/slide' + number + '.html';
+    previousButton = (`
+        <img src="img/next.svg" alt="previous image" id="previous" title="previous" onclick="previous()">
+        <div class="elements">
+    `);
+    nextButton = (`
+        </div>
+        <img src="img/next.svg" alt="next image" id="next" title="next" onclick="next()">`
+    );
+    fetch(slideFile)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Slide not found');
+            }
+            return response.text();
+        })
+        .then(html => {
+            if(slideCount == 0){
+                document.getElementById('slide').innerHTML = html;
+            } else{
+            document.getElementById('slide').innerHTML = previousButton + html + nextButton;
+            }
+        })
+        .catch(error => {
+            console.error('Error loading slide:', error);
+            document.getElementById('slide').innerHTML = '<p>Slide not found</p>';
+        })
+    ;
+}
+
+
+
+
+
+//SLIDE1
+function color(colorChange){
+    document.getElementById('change').style.color = (colorChange);
+}
+
+//SLIDE2
+function background_color(background_colorChange, colorChange){
+    document.getElementById('change').style.backgroundColor = (background_colorChange);
+    document.getElementById('change').style.color = (colorChange);
+}
+
+//SLIDE3
+function font_style(font_styleChange){
+    document.getElementById('change').style.fontStyle = (font_styleChange);
+}
+
+//SLIDE4
+function font_size(font_sizeChange){
+    document.getElementById('change').style.fontSize = (font_sizeChange);
+}
